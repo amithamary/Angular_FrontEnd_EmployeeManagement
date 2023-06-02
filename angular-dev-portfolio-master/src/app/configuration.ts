afca5410907394b031f2aca3f86852422ece7840
amithamary <amithamarybenny@gmail.com>
2023-06-02 20:13:42 +0530

    first commit

---
+++ angular-dev-portfolio-master/src/app/configuration.ts
@@ -0,0 +1,69 @@
+export const Configuration =
+{
+    profileUrl:"https://media.licdn.com/dms/image/C5603AQGPp8e4Lbhoew/profile-displayphoto-shrink_800_800/0/1585143845080?e=2147483647&v=beta&t=Pc7P2nCGgBk9U5A9JLcw47IsDvud623-Ph1h4slveR4",
+    userName:"Amitha Mary Benny",
+    userJob:"Software Developer",
+    userLocation:"Ernakulam",
+    userEmail:"245166@ust.com",
+    userDateOfBirth:"26/01/1998",
+    userAddress:"Ernakulam",    
+    userPhone:"8078538488",
+    aboutMe:"Amitha Mary Benny. I am fullstack developer. I am interested in advanced technologies like AI,Data science,Cloud.",
+};
+
+//Personal Informations
+
+export const Skills  = 
+[
+    "Java","HTML","CSS","SQL","Javascript"
+];
+
+//Max skills limit is 6 . If you add more than 6 skill , you may be disappointed.
+
+export const JobExperience  = 
+[
+    {company : "IBM / Serii" , rank : "Logistic Support Personal " , date:"07/2017 - 08/2016" , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    {company : "Albayrak Bilgisayar" , rank : "Technical Service Personal" , date:"07/2015 - 07/2015" , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+];
+
+//Max job experiences limit is 3 . If you add more than 3 skill , you may be disappointed.
+
+export const Education  = 
+[
+    { school : "Sivas Cumhuriyet University" , date:"09/2018 - Present" , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    { school : "Türk Telekom Vocational High School" , date:"09/2013 - 07/2017" , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+];
+
+//Max education limit is 3 . If you add more than 3 skill , you may be disappointed.
+
+export const Services  = 
+[
+    { serviceName : "Web Design" ,  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    { serviceName : "Desktop Development"  , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    { serviceName : "Software Architecture"  , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    { serviceName : "Social Media Management"  , description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},
+    { serviceName : "Logo Design" ,  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+];
+
+//Max services limit is 6 . If you add more than 6 skill , you may be disappointed.
+
+export const Testimonials  = 
+[
+    { name : "Person X" , company:"Facebook" ,  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam aliquet dui at ultricies. Donec fermentum lorem sit amet massa vestibulum, vel pellentesque lectus vestibulum." , review:85},
+    { name : "Person Y" , company:"Instagram" ,  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam aliquet dui at ultricies. Donec fermentum lorem sit amet massa vestibulum, vel pellentesque lectus vestibulum." , review:80},
+    { name : "Person Z" , company:"Uber" ,  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam aliquet dui at ultricies. Donec fermentum lorem sit amet massa vestibulum, vel pellentesque lectus vestibulum." , review:79},
+    { name : "Person W" , company:"Airbnb" ,  description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquam aliquet dui at ultricies. Donec fermentum lorem sit amet massa vestibulum, vel pellentesque lectus vestibulum." , review:99}
+
+];
+
+//Max testimonials limit is 4 . If you add more than 4 skill , you may be disappointed.
+
+export const SocialMedia =
+{
+    github:"github.com",
+    instagram:"instagram.com",
+    twitter:"twitter.com",
+    linkedin:"linkedin.com",
+    medium:"medium.com",
+    facebook:"facebook.com"
+}
